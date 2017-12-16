62ccdec419c2e5d814e749f23fad798861427c2f
Vithrandir <2541556282@qq.comm>
2017-12-16 20:25:53 +0800

    1.0

--- open.c
+++ open.c
@@ -1,6 +1,9 @@
 #include "stdio.h"
 #include "fcntl.h"
 
+char buf1[] = "abcdefghij";
+char buf2[] = "ABCDEFGHIJ";
+
 int main(int argc, char const *argv[])
 {
 	int a;
@@ -15,6 +18,35 @@ int main(int argc, char const *argv[])
 	int e;
 	e = lseek(4,1,SEEK_CUR);
 	printf("%d\n",e);
+
+	int fd;
+	if ((fd = creat("file.hole",FILESEC_MODE)) < 0)
+	{
+		err_sys("creat error");
+	}
+
+	if (write(fd,buf1,10) != 10)
+	{
+		err_sys("buf1 write error");
+	}
+
+	if (lseek(fd,16384,SEEK_SET) == -1)
+	{
+		err_sys("lseek error");
+	}
+
+	if (write(fd,buf2,10) != 10)
+	{
+		err_sys("buf2 write error");
+	}
+	// if (lseek(STDIN,0,SEEK_CUR) == -1)
+	// {
+		// printf("can not seek\n");
+	// }
+	// else{
+		// printf("seek ok\n");
+	// }
+
 	// int d;
 	// d = close(3);
 	// printf("%d\n",d);
